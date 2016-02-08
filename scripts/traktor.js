d06276ae01114ab287980f4b777177fc2b15ea65
SevaXXL <adeinega@mail.ru>
2016-02-08 11:24:46 +0300

    Traktor Pro script

--- scripts/traktor.js
+++ scripts/traktor.js
@@ -6,17 +6,18 @@ var needInit = true;
 var datafile = __dirname + '/' + 'NowPlaying.txt';
 
 net.createServer(function (sock) {
-	console.log('Conected');
 	sock.on('data', function (dataChunk) {
 		if (needInit) {
 			sock.write('HTTP/1.0 200 OK\r\n\r\n');
+			console.log('Conected');
 			needInit = false;
 		} else {
 			var title = getName('title', dataChunk);
 			var artist = getName('artist', dataChunk);
 			if (title || artist) {
-				console.log(artist + ' - ' + title);
-				fs.writeFile(datafile, 'Title: ' + (title || '') + '\nArtist: ' + (artist || ''));
+				fs.writeFile(datafile, 'Title: ' + (title || '') + '\nArtist: ' + (artist || ''), function () {
+					console.log(artist + ' - ' + title);
+				});
 			}
 		}
 	});
@@ -32,11 +33,12 @@ net.createServer(function (sock) {
  * Search in Buffer
  */
 function getName(needle, haystack) {
+	// (....)TITLE=(.*) - $1: размер, $2: значение
 	needle = needle.toUpperCase() + '=';
 	var position = haystack.indexOf(needle);
 	if (position != -1) {
 		var length = haystack.slice(position - 4, position);
-		length = bufferpack.unpack('<l', length)[0];
-		return haystack.toString('utf8', position + needle.length, position + length);
+		length = bufferpack.unpack('<l', length);
+		return haystack.toString('utf8', position + needle.length, position + length[0]);
 	}
 }
